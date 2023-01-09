3a5b8c55aa2f3c999c06c4622b03b3c161e0be45
Wade Huber <wadehuber@gmail.com>
2023-01-08 21:22:07 -0700

    First project starter code

---
+++ csc110examples/src/projects/project1/HelloCSC110.java
@@ -0,0 +1,11 @@
+package projects.project1;
+
+public class HelloCSC110 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, world!");
+
+	}
+
+}
---
+++ csc110examples/src/projects/project2/ExamAverage.java
@@ -0,0 +1,10 @@
+package projects.project2;
+
+public class ExamAverage {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
