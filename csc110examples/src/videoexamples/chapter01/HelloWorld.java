6e50966ab52510c4b0be83f8dbbb9898f1704c11
Wade Huber <wadehuber@gmail.com>
2023-01-19 08:14:28 -0700

    Reverted poor whitespace

--- csc110examples/src/videoexamples/chapter01/HelloWorld.java
+++ csc110examples/src/videoexamples/chapter01/HelloWorld.java
@@ -1,16 +1,11 @@
-package 
-videoexamples.chapter01;
+package videoexamples.chapter01;
 
-public 			class 
-HelloWorld 				{ public static 
-	void 			main(String
-			[] 
-			
-			
-			args) { 
-		System.out.println
+public class HelloWorld {
 
-("Hello, world!"
-		) ; }
+	public static void main(String[] args) {
+
+		System.out.println("Hello, world!");
+
+	}
 
 }
