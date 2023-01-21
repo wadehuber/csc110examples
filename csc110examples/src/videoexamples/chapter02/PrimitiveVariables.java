d5cbff15ffe5abd80c69bfd3ad5ab17475745a75
Wade Huber <wadehuber@gmail.com>
2023-01-21 14:47:11 -0700

    Sync with video

--- csc110examples/src/videoexamples/chapter02/PrimitiveVariables.java
+++ csc110examples/src/videoexamples/chapter02/PrimitiveVariables.java
@@ -3,23 +3,38 @@ package videoexamples.chapter02;
 public class PrimitiveVariables {
 
 	public static void main(String[] args) {
-		int num;  
-		int val = 12;
-		double precise = 3.9343;  
+		
+		int num;        // 32-bit integer
+		double precise; // 64-bit floating point number
+		boolean result; // Boolean type
+		char letter;    // 16-bit Unicode character
+		
+		byte loc;       // 8-bit integer
+		short small;    // 16-bit integer
+		long big;       // 64-bit integer
+		float flt;      // 32-bit floating point number
+		
+		// Initializing declarations
+		double real = 12345.6789;
+		int year = 365;
 		boolean decide = true;
 		boolean thisIsPython = false;
 		char grade = 'A';
-		
-		byte loc;
-		short smallNum = 12345;
 		long bigNum = 123456789012345L;
 		float pi = 3.14159F;
-
+		
 		// Multiple variables in a single declaration
 		int x, y, z;
-		double r, n, k;
-		char excellent = 'A', ok = 'C', fail = 'F';
+		double k, n, r;
+		char excellent = 'A', ok = 'C', fail = 'F', mine;
 		boolean start = false, done;
+		
+		// Initializing a variable with an expression
+		int a = 10;
+		int b = 42;
+		int c = a * b + 17;
+		
+		
 	}
 
 }
