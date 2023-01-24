b777b1c0692223e838cc536fd5a22f2cb6a20e22
Wade Huber <wadehuber@gmail.com>
2023-01-24 08:26:42 -0700

    Added example

---
+++ csc110examples/src/videoexamples/module02/ScannerInput.java
@@ -0,0 +1,14 @@
+package videoexamples.module02;
+
+public class ScannerInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int t1 =10;
+		 int t2 = 20;
+		 
+		 if (t1 == t2);
+		 
+	}
+
+}
