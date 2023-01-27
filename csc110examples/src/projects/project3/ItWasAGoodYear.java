95382da629eec013e2167161cd65e3c1ad942488
Wade Huber <wadehuber@gmail.com>
2023-01-27 15:51:49 -0700

    Initial project 3 starter

---
+++ csc110examples/src/projects/project3/ItWasAGoodYear.java
@@ -0,0 +1,13 @@
+package projects.project3;
+
+import java.util.Scanner;
+
+public class ItWasAGoodYear {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		System.out.println("What year would you like to check?");
+		
+	}
+}
