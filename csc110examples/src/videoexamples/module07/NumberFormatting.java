c0f20b1fe1ce219ace84e63c5f0ba2f4dd51dafc
Wade Huber <wadehuber@gmail.com>
2023-03-02 18:35:24 -0700

    Sync with video

--- csc110examples/src/videoexamples/module07/NumberFormatting.java
+++ csc110examples/src/videoexamples/module07/NumberFormatting.java
@@ -7,28 +7,27 @@ public class NumberFormatting {
 	final static double INCOME_TAX_RATE = 0.20;  // 20% income tax
 
 	public static void main(String[] args) {
-	
+
+		NumberFormat percentFmt = NumberFormat.getPercentInstance();
+		NumberFormat moneyFmt = NumberFormat.getCurrencyInstance();
 
 		int salary = 75000;
-		int hourlyWage = 16;
 		
-		NumberFormat moneyFmt = NumberFormat.getCurrencyInstance();
-		NumberFormat percentFmt = NumberFormat.getPercentInstance();		
-
 		double weeklyWage = salary / 52;
 		double taxes = weeklyWage * INCOME_TAX_RATE;
 		
 		// Print weekly pay for salaried employee
 		
-		// Without NumberFormat:
-	    // System.out.println("Yearly salary: " + salary);
-	    // System.out.println("Weekly pay: " + weeklyWage);
-	    // System.out.println("Taxes: " + taxes + " at " + INCOME_TAX_RATE);
-
+		// Without NumberFormat
+	    //System.out.println("Yearly salary: " + salary);
+	    //System.out.println("Weekly pay: " + weeklyWage);
+	    //System.out.println("Taxes: " + taxes + " at " + INCOME_TAX_RATE);
+	    
 	    // With NumberFormat
 	    System.out.println("Yearly salary: " + moneyFmt.format(salary));
 	    System.out.println("Weekly pay: " + moneyFmt.format(weeklyWage));
 	    System.out.println("Taxes: " + moneyFmt.format(taxes) + " at "
-	                        + percentFmt.format(INCOME_TAX_RATE));
+	                         + percentFmt.format(INCOME_TAX_RATE));
+
 	}
 }
