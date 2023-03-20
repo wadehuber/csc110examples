5f73dd9cc2f00f41bd2c6f0e6c6412a888255c3c
Wade Huber <wadehuber@gmail.com>
2023-03-19 21:10:49 -0700

    Added method exmaples

---
+++ csc110examples/src/videoexamples/module10methods/MethodsIntro.java
@@ -0,0 +1,29 @@
+package videoexamples.module10methods;
+
+public class MethodsIntro {
+
+	public static double mean(int[] values) {
+		int sum = 0;
+		double average;
+		for (int ii=0;ii<values.length;ii++) {
+			sum += values[ii];
+		}
+		return (double) sum / values.length;
+	}
+	
+	public static void main(String[] args) {
+		int[] numbers = {10, 17, 13, 24, 9};
+		
+		int sum = 0;
+		double average;
+		for (int ii=0;ii<numbers.length;ii++) {
+			sum += numbers[ii];
+		}
+		average = (double) sum / numbers.length;
+		System.out.println("Average = " + average);
+		System.out.println("Mean = " + mean(numbers));
+	
+
+	}
+
+}
---
+++ csc110examples/src/videoexamples/module10methods/README.md
@@ -0,0 +1,3 @@
+# Module 10 - Methods
+
+- Methods.java [Video](https://youtu.be/0WRJgPZQ3Gc) (8:19): Example of writing methods in Java (note that in the video the class is called "Functions", but the content is the same)
