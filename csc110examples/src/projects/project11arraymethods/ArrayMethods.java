166324d644e2f5c5a35d65e5b4e91717fd7dd704
Wade Huber <wadehuber@gmail.com>
2023-03-23 12:31:51 -0700

    Starter code for Project 11

---
+++ csc110examples/src/projects/project11arraymethods/ArrayMethods.java
@@ -0,0 +1,36 @@
+package projects.project11arraymethods;
+
+public class ArrayMethods {
+
+	public static void main(String[] args) {
+	
+		/*
+		DELETE THIS AND THE PREVIOUS LINE
+
+		System.out.println("Testing display (part 1):");
+		display(labels, values);
+
+		System.out.println("\nTesting display (part 2):");
+		display(labels, values, 1);
+		display(labels, values, 2);
+
+		System.out.println("\nTesting statistics:");
+		System.out.printf("Min = %.2f\n", findMin(values));
+		System.out.printf("Min = %.2f\n", findMin(values));
+
+		System.out.printf("Total = %.2f\n", calculateTotal(values));
+		System.out.printf("Average = %.2f\n", calculateAverage(values));
+
+		System.out.println("\nTesting countValues:");
+		// Change the parameter here so that a different result is 
+		//   returned each time
+		System.out.println("Values > 15 = " + countValues(values, 15.0));
+		System.out.println("Values > 20 = " + countValues(values, 20.0));
+		System.out.println("Values > 30 = " + countValues(values, 30.0));
+		
+		DELETE THIS AND THE NEXT LINE
+		*/
+		
+	}
+
+}
