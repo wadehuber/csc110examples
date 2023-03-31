7f1f15a9c9aa68b468c083a17153a706a314dde5
Wade Huber <wadehuber@gmail.com>
2023-03-30 20:14:51 -0700

    Test driver for Project 12

---
+++ csc110examples/src/projects/project12/Project12Driver.java
@@ -0,0 +1,57 @@
+package projects.project12;
+
+public class Project12Driver {
+
+    public static void main(String[] args) {
+    	System.out.println("Project 12 Sample Output for YOUR NAME");
+
+    	/*
+        Stock[] stocks = {new Stock("LittleCorp", "LTLC", 15.73, 15.39, 200),
+                          new Stock("Monsters, Inc", "SCAR", 52.26, 55.07, 1000000),
+                          new Stock("Big Business", "BIGB", 139.44, 142.51, 100000000)
+        };
+
+        System.out.println("Test print method");
+        for(int ii=0;ii<stocks.length;ii++) {
+        	System.out.print("  ");  // indent
+            stocks[ii].print();
+        }
+        System.out.println();
+
+        System.out.println("Test company getter & marketCap method");
+        for(int ii=0;ii<stocks.length;ii++) {
+            System.out.println("  " + stocks[ii].getCompany() 
+                    + " is worth " + stocks[ii].marketCap());
+        }
+        System.out.println();
+
+        System.out.println("Test buy method");
+        for(int ii=0;ii<stocks.length;ii++) {
+            if(stocks[ii].buy(20.0)) {
+                System.out.println("  " + "BUY " + stocks[ii].getCompany());
+            }
+            else {
+                System.out.println("  " + "HOLD " + stocks[ii].getCompany());
+            }
+        }
+        System.out.println();
+        
+        System.out.println("Test result setter method");
+        for(int ii=0;ii<stocks.length;ii++) {
+        	System.out.print("  ");  // indent
+        	stocks[ii].result();
+        }
+        System.out.println();
+
+        System.out.println("Test closing price setter method");
+        stocks[0].setClosingPrice(17.94);
+        stocks[2].setClosingPrice(135.22);
+
+        for(int ii=0;ii<stocks.length;ii++) {
+        	System.out.print("  ");  // indent
+            stocks[ii].print();
+        }
+        */
+	}
+
+}
