57b47ee255c9f5dea476cc8c4fa102379474ca21
Wade Huber <wadehuber@gmail.com>
2023-04-16 20:03:54 -0700

    Added comments with directions

--- csc110examples/src/projects/project14/Project14Driver.java
+++ csc110examples/src/projects/project14/Project14Driver.java
@@ -1,10 +1,23 @@
 package projects.project14;
 
 public class Project14Driver {
+    public static void main(String[] args) {
+        // Instantiate a Television object
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Instantiate a Laptop object
 
-	}
+        // Display information about the devices
+        
+        // Change to HDMI1 on the television
 
+        // Open Microsoft Word on the laptop
+
+        // Display information about the devices
+
+        // Open Eclipse on the laptop
+
+        // Change to Netflix on the television
+
+        // Display information about the devices
+    }
 }
