56327c1ed313da974ffff3c902d2e56126de856d
Wade Huber <wadehuber@gmail.com>
2024-01-15 14:29:59 -0700

    Formatting

--- csc110examples/src/labs/Module01Lab.java
+++ csc110examples/src/labs/Module01Lab.java
@@ -11,10 +11,10 @@ public class Module01Lab {
 		System.out.println("CSC110 Lab 1");
 		System.out.println();
 		
-        // Print "Hello, CSC110", ending with a newline
+		// Print "Hello, CSC110", ending with a newline
 
 				
-        // Print your name, favorite book/movie, hobby, & favorite color
+		// Print your name, favorite book/movie, hobby, & favorite color
 		
 
 		// Print "Effort creates opportunity" inside a box on 3 lines
