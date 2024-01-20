b9a9c90039f4b49c297e7dd98a6a363f4d587a16
Wade Huber <wadehuber@gmail.com>
2024-01-20 10:48:57 -0700

    Updated Module 2 starter

---
+++ csc110examples/src/labs/Module02Lab.java
@@ -0,0 +1,45 @@
+package labs;
+
+
+public class Module02Lab {
+
+	public static void main(String[] args) {
+
+		String scifi = "Science";
+
+		// Display the string "Computer Science" two ways
+
+		// Write code that reads two integers num1 & num2 then prints their sum &
+		// product
+
+		System.out.print("Enter 2 integers: ");
+
+		// Write code that reads three integers num, val, and mult, then outputs the
+		// result of adding num & val together then multiplying that sum by mult.
+
+		System.out.print("Enter three integers: ");
+
+		// Write code that reads an integer named start then prints the square (n*n)
+		// cube (n*n*n) of start.
+
+		System.out.print("Enter a number (start): ");
+
+		// Write code that reads in 4 numbers: val1, val2, val3, & val4 then calculates
+		// the average (mean) of the 4 numbers.
+
+		System.out.print("Enter 4 numbers: ");
+
+		// Write code that reads in 4 numbers representing the number of quarters,
+		// dimes, and nickels & pennies someone has, then calculates the total
+		// monetary value of the coins. Display the total number of coins
+		// followed by the total monetary value.
+
+		// Read in a name, number, thing & place, then print the sentence
+		// NAME has brought a THING to PLACE NUMBER times
+		// replacing NAME, THING, PLACE, & NUMBER with the inputs
+
+		System.out.println("Enter a name, number, thing, & place: ");
+
+	}
+
+}
