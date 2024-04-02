1c730be0d043e37adcc69e710a693d826b4c32cb
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2024-04-02 11:08:11 -0700

    Spring 2024 Array example

---
+++ csc110examples/src/classexamples/ArrayExamples.java
@@ -0,0 +1,43 @@
+package classexamples;
+
+import java.util.Random;
+
+public class ArrayExamples {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+
+		double[] prices = new double[5];
+		int[] nums = new int[10];
+		char[] letterGrades = {'A', 'B', 'C', 'D', 'F'};
+		String[] names = {"Lucas", "Chana", "Tucker", "Jaydan", "Halle", "Angel"};
+				
+		
+		for (int ii=0;ii<prices.length;ii++) {
+			prices[ii] = Math.random() * 100;
+		}
+		
+		for (int ii=0;ii<prices.length;ii++) {
+			System.out.printf("%.2f ", prices[ii]);
+		}
+		System.out.println("\n");
+		
+		for (int ii=0;ii<nums.length;ii++) {
+			nums[ii] = rand.nextInt(100) + 1;
+		}
+
+		for (int num : nums) {
+			System.out.print(num + " ");
+		}
+		
+		System.out.println("\n");
+		for (int ii=0;ii<names.length;ii++) {
+			int gradeIndex = rand.nextInt(letterGrades.length);
+			System.out.println(names[ii] + "'s grade is " + letterGrades[gradeIndex]);
+		}
+		
+
+
+	}
+
+}
