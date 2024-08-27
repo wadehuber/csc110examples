a8da59d1f443e60020ad9eef41418d11bed9a99c
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2024-08-27 11:27:39 -0700

    Spring 2024 changes

---
+++ csc110examples/src/classexamples/classexample/module1/PrintPrintln.java
@@ -0,0 +1,12 @@
+package classexamples.classexample.module1;
+
+public class PrintPrintln {
+
+	public static void main(String[] args) {
+		System.out.print("Example: ");
+		System.out.println("CSC110AB Thursday, August 22");
+		System.out.print("The End ");
+
+	}
+
+}
