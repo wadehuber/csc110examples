23d685ac1e35d169baef4d08cbd88256840aeebc
Wade Huber <wadehuber@gmail.com>
2024-09-15 18:53:25 -0700

    Sync with video

--- csc110examples/src/videoexamples/loops/ForLoop.java
+++ csc110examples/src/videoexamples/loops/ForLoop.java
@@ -6,21 +6,15 @@ public class ForLoop {
 	public static void main(String[] args) {
 	
 		Scanner scnr = new Scanner(System.in);
-		int num1, num2, num3, num4, num5;
-		int total;
+		int num;
+		int total = 0;
 		
-		System.out.print("Enter number 1: ");
-		num1 = scnr.nextInt();
-		System.out.print("Enter number 2: ");
-		num2 = scnr.nextInt();
-		System.out.print("Enter number 3: ");
-		num3 = scnr.nextInt();
-		System.out.print("Enter number 4: ");
-		num4 = scnr.nextInt();
-		System.out.print("Enter number 5: ");
-		num5 = scnr.nextInt();
+		for(int ii=0; ii<5; ii++) {
+			System.out.print("Enter number : ");
+			num = scnr.nextInt();
+			total = total + num;
+		}
 	
-		total = num1 + num2 + num3 + num4 + num5;
 		System.out.println("The total is " + total);
 		
 		scnr.close();
