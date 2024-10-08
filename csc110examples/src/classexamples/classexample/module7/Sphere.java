6863de18e7098966af867298766fac7c5152bb46
Wade Huber <wadehuber@gmail.com>
2024-10-07 21:22:42 -0700

    Added sphere example

---
+++ csc110examples/src/classexamples/classexample/module7/Sphere.java
@@ -0,0 +1,22 @@
+package classexamples.classexample.module7;
+
+public class Sphere {
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+
+    public double getSurfaceArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+
+}
---
+++ csc110examples/src/classexamples/classexample/module7/SphereDriver.java
@@ -0,0 +1,20 @@
+package classexamples.classexample.module7;
+
+import java.util.Scanner;
+
+public class SphereDriver {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Welcome to the Sphere Calculator." );
+        System.out.print ("Enter the sphere's radius: ");
+   
+        Sphere mySphere = new Sphere(sc.nextDouble());
+        System.out.println("Radius: " + mySphere.getRadius());
+        System.out.println("Volume: " + mySphere.getVolume());
+        System.out.println("Surface Area: " + mySphere.getSurfaceArea());
+        
+        sc.close();
+    }
+
+}
