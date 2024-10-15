d5d409d7a2ecf0c4cd00f5f0e4bc2d85b3f54a41
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2024-10-15 10:29:03 -0700

    Money example Fall 2024

---
+++ csc110examples/src/classexamples/classexample/module7/Money.java
@@ -0,0 +1,26 @@
+package classexamples.classexample.module7;
+
+public class Money {
+	
+	int amount;   // this is in "cents", not dollars
+	String currency;
+	
+	// Constructor 
+	public Money(int a, String c) {
+		super();
+		amount = a;
+		currency = c;
+	}
+
+	@Override
+	public String toString() {
+		if (amount >= 100) {
+			return "Money [amount=" + amount/100 + ", currency=" + currency + "]";
+		}
+		else {
+			return "Money [amount=" + amount + ", currency=Cents" + "]";
+		}
+
+	}
+	
+}
---
+++ csc110examples/src/classexamples/classexample/module7/MoneyDriver.java
@@ -0,0 +1,14 @@
+package classexamples.classexample.module7;
+
+public class MoneyDriver {
+
+	public static void main(String[] args) {
+		Money bill = new Money(500, "Dollars");
+		Money coin = new Money(25, "Dollars");
+		
+		System.out.println(bill);
+		System.out.println(coin);
+
+	}
+
+}
