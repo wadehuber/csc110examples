e40334ae9b82799f63b3ba507c047e25da0bd6ee
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2024-10-22 11:27:26 -0700

    Fall 2024 example

--- csc110examples/src/classexamples/M9ArrayExamples.java
+++ csc110examples/src/classexamples/M9ArrayExamples.java
@@ -6,38 +6,61 @@ public class M9ArrayExamples {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-
-		double[] prices = new double[5];
+		
+		// Declaring arrays
 		int[] nums = new int[10];
+		double[] prices = new double[5];
+		
 		char[] letterGrades = {'A', 'B', 'C', 'D', 'F'};
 		String[] names = {"Lucas", "Chana", "Tucker", "Jaydan", "Halle", "Angel"};
-				
 		
-		for (int ii=0;ii<prices.length;ii++) {
+		// Fill array with a loop
+		for(int ii=0; ii<prices.length; ii++) {
 			prices[ii] = Math.random() * 100;
 		}
-		
-		for (int ii=0;ii<prices.length;ii++) {
-			System.out.printf("%.2f ", prices[ii]);
+		// Print array with a loop
+		System.out.println("Prices:");
+		for(int ii=0; ii<prices.length; ii++) {
+			System.out.printf("  $%.2f\n", prices[ii]);
 		}
-		System.out.println("\n");
 		
-		for (int ii=0;ii<nums.length;ii++) {
+		// Initialize & print in same loop
+		System.out.print("Nums: ");
+		for(int ii=0;ii<nums.length; ii++) {
 			nums[ii] = rand.nextInt(100) + 1;
+			System.out.print(nums[ii] + " ");
 		}
-
-		for (int num : nums) {
-			System.out.print(num + " ");
+		System.out.println();
+		
+		// Gradebook - associating two arrays of different sizes
+		System.out.println();
+		System.out.println("Gradebook:");
+		for (int ii=0; ii<names.length; ii++) {
+			int gradeIndex=rand.nextInt(letterGrades.length);
+			System.out.println("  " + names[ii] + "'s grade is " + letterGrades[gradeIndex]);
 		}
 		
-		System.out.println("\n");
-		for (int ii=0;ii<names.length;ii++) {
-			int gradeIndex = rand.nextInt(letterGrades.length);
-			System.out.println(names[ii] + "'s grade is " + letterGrades[gradeIndex]);
-		}
+		// 2 letter grade deduction
+		int gIndex=0;
+		deductLetterGrade(gIndex, letterGrades);
+		gIndex=2;
+		deductLetterGrade(gIndex, letterGrades);
+		gIndex=3;
+		deductLetterGrade(gIndex, letterGrades);
 		
-
-
+		// For-each loop
+		System.out.println();
+		System.out.print("Nums (for-each): ");
+		for (int value : nums) {
+			System.out.print("  " + value);
+		}
+		System.out.println();
+	}
+	
+	public static void deductLetterGrade(int gradeIndex, char grades[]) {
+		int newIndex=Math.min(gradeIndex+2, grades.length-1);
+		System.out.print("Original grade: " + grades[gradeIndex]);
+		System.out.println("    Grade after 2 letter grade deduction: " + grades[newIndex]);
 	}
 
 }
