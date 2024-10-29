ac5389c464095974662a5637f0c475bbf03c5560
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2024-10-29 11:05:56 -0700

    Initial version

---
+++ csc110examples/src/classexamples/classexample/M10TwoDArrays.java
@@ -0,0 +1,41 @@
+package classexamples.classexample;
+
+public class M10TwoDArrays {
+
+	public static void main(String[] args) {
+		int[][] code = new int[8][10];
+		int num = 1;
+		
+		for(int ii=0;ii<code.length;ii++) {
+			for(int jj=0;jj<code[ii].length;jj++) {
+				code[ii][jj] = num;
+				num++;
+			}
+		}
+
+		for(int ii=0;ii<code.length;ii++) {
+			for(int jj=0;jj<code[ii].length;jj++) {
+				System.out.printf("%4d", code[ii][jj]);
+			}
+			System.out.println();
+		}
+
+		
+		System.out.println();
+		num=1;
+		for(int jj=0;jj<code[0].length;jj++) {
+			for(int ii=0;ii<code.length;ii++) {
+				code[ii][jj] = num;
+				num++;
+			}
+		}
+
+		for(int ii=0;ii<code.length;ii++) {
+			for(int jj=0;jj<code[ii].length;jj++) {
+				System.out.printf("%4d", code[ii][jj]);
+			}
+			System.out.println();
+		}
+	}
+
+}
