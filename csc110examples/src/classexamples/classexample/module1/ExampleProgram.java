80e43363080a849510e3302d4304871ce91ea0e9
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2025-01-16 11:04:56 -0700

    Spring 2025 example

---
+++ csc110examples/src/classexamples/classexample/module1/ExampleProgram.java
@@ -0,0 +1,23 @@
+package classexamples.classexample.module1;
+
+public class ExampleProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.print("System.out.print:");
+		System.out.print("Hello");
+		System.out.print(", ");
+		System.out.print("world!");
+		
+		System.out.print("\n");    // \n is the "newline" character
+		System.out.println();      // println goes to the next line
+		
+
+		System.out.println("System.out.println:");
+		System.out.println("Hello");
+		System.out.println(", ");
+		System.out.println("world!");
+
+	}
+
+}
