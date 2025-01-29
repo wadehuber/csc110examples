aaf087c0959fdfdaae1d027170fb669686301b80
Wade Huber <wadehuber@gmail.com>
2025-01-29 13:29:38 -0700

    Initial version

---
+++ csc110examples/src/videoexamples/dataexpressions/IntegerDivision.java
@@ -0,0 +1,12 @@
+package videoexamples.dataexpressions;
+
+public class IntegerDivision {
+
+	public static void main(String[] args) {
+		
+		System.out.println("15 / 5 = " + (15/5));
+		System.out.println("24 / 4 = " + (24/4));
+		
+	}
+
+}
