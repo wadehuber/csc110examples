aaec72f885a2ea0ce8faf8130364e14b39620074
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2025-03-18 12:39:13 -0700

    Spring 2025 Example

--- csc110examples/src/classexamples/module8classdesign/book/BookDriver.java
+++ csc110examples/src/classexamples/module8classdesign/book/BookDriver.java
@@ -3,18 +3,20 @@ package classexamples.module8classdesign.book;
 public class BookDriver {
 
 	public static void main(String[] args) {
-		// Create a Book object
+
+		// Create (instantiate) Book objects (instances)
 		Book myBook = new Book("Pride and Prejudice", "Jane Austen", "Novel", 448);
 		Book yourBook = new Book("Starship Troopers", "Robert A. Heinlein", "Novel", 299);
-
-		// Use the methods
-		System.out.println("my book:");
+		
+		// Use the methods ("drive" the class)
+		System.out.println("myBook: " + myBook); // this uses the toString method
+		myBook.printInfo();
 		myBook.read();
-		myBook.getInfo();
 		
 		System.out.println();
-		System.out.println("Your book:");
+		System.out.println("yourBook: " + yourBook); // this uses the toString method
+		yourBook.printInfo();
 		yourBook.read();
-		yourBook.getInfo();
 	}
+
 }
