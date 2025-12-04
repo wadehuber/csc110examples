78a6451e2fef18ffed9a71403733a414acd8b1a6
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2025-12-04 11:04:07 -0700

    File 2025 example

--- csc110examples/src/classexamples/module11inheritance/Payroll.java
+++ csc110examples/src/classexamples/module11inheritance/Payroll.java
@@ -4,31 +4,49 @@ public class Payroll {
 
 	public static void main(String[] args) {
 		
-		Employee emp1 = new Employee("Rachel Waashington", 80000);
-
-		HourlyEmployee emp2 = new HourlyEmployee("Chicken Joe", 17.15);
-		emp2.setHoursWorked(25);
+		Employee emp1 = new Employee("Rachel Washington", 80000);
+		Volunteer emp2 = new Volunteer("Margaret Simpson");
+		HourlyEmployee emp3 = new HourlyEmployee("Kenny Southpark", 16);
+		emp3.setHoursWorked(20);
+		emp3.addHoursWorked(20);
 		
-		Volunteer emp3 = new Volunteer("Harry Potter");
 		
-		
-		System.out.println("emp1: " + emp1 + " paid " + emp1.pay());
-		System.out.println("emp2: " + emp2 + " paid " + emp2.pay());
-		System.out.println("emp3: " + emp3 + " paid " + emp3.pay());
-		
-		// Polymorphism example
+		System.out.print(emp1 + " paid $");
+		System.out.printf("%.2f", + emp1.pay());
 		System.out.println();
-		System.out.println("Company E");
+		
+		System.out.print(emp2 + " paid $");
+		System.out.printf("%.2f", + emp2.pay());
+		System.out.println();
+		
+		System.out.print(emp3 + " paid $");
+		System.out.printf("%.2f", + emp3.pay());
+		System.out.println();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Company payroll:");
+		HourlyEmployee emp4 = new HourlyEmployee("Anne Elliot", 22);
+		emp4.setHoursWorked(32);
+
 		Employee[] company = {
-				new Employee("Elizabeth Sorenson", 120000),
-				new Volunteer("Jason Wentworth"),
-				new HourlyEmployee("Thailia Elliot", 25),
-				new HourlyEmployee("Leo Bertram", 15),
-				new Employee("Reyna Lucas", 75000)
+				emp1, 
+				emp2, 
+				emp3,
+				new Employee("Frederick Wentworth", 120000),
+				emp4,
+				new Volunteer("Lady Russell"),
 		};
+		
+
 		for (Employee e : company) {
-			System.out.println(e + " paid " + e.pay());
+			System.out.print("   " + e + " paid $");
+			System.out.printf("%.2f", + e.pay());
+			System.out.println();
 		}
+	
+		
+		
 		
 	}
 
