2200c2a542cdcdb3a2217d9b57e0cca0ea746c0c
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2026-02-17 11:18:56 -0700

    Spring 2026 Example

--- csc110examples/src/classexamples/module05loops/Loops.java
+++ csc110examples/src/classexamples/module05loops/Loops.java
@@ -8,11 +8,11 @@ public class Loops {
 	public static void main(String[] args) {
 		
 		Random rand = new Random();
-		//Scanner scan = new Scanner(System.in);
-		//System.out.print("Enter a number: ");
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a number: ");
 		//int limit = scan.nextInt();
-		int limit = 11;
-	
+		int limit = 9;
+		
 		// While Loop - repeats while a condition is true
 		System.out.println("While loop:");
 		int num = 1;
@@ -22,70 +22,95 @@ public class Loops {
 		}
 		System.out.println();
 		
-		System.out.println("While loop running total:");
-		int total = 0;
+		// Keep a running total
+		System.out.println("While Loop - running total:");
 		num = 1;
+		int total = 0;
 		while (num <= limit) {
 			total += num;
-				++num;
-		}
-		System.out.println("  The total is: " + total);
-		
-		System.out.println("While loop until condition is met:");
-		total = 0;
-		int tries = 1;
-		while (total <= 1000) {
-			num = rand.nextInt(100);
-			System.out.print(" " + num);
-			total += num;   // Keeps track of the total
-			tries ++;       // Keeps track of how many random numbers
-		}
-		System.out.println("\n  The total is: " + total + " (" + tries + ")");
-		
-		// Do-While Loop (or Do Loop) - do something, then repeat while a condition is true
-		num = 1;
-		System.out.println("Do-While Loop: ");
-		do {
-			System.out.print(" " + num);
 			++num;
-		} while (num <= limit);
+		}
+		System.out.println("The total is: " + total);
 		System.out.println();
 		
-		// For Loop: for X = 1 to limit
-		System.out.println("For Loop: ");
-		for (int ii=0; ii<10; ii++) {
+		// While Loop until a condition is met
+		int tries = 1;
+		// Check that the limit is valid before we begin
+		if ((limit >= 1) && (limit <= 1000)) {
+			int guess = rand.nextInt(1000) + 1;
+			while (guess != limit) {
+				tries ++;
+				guess = rand.nextInt(1000) + 1;
+			}
+			System.out.println("It took " + tries + " tries to guess " + limit);
+		}
+		
+		// Nested conditional in a loop
+		num = 1;
+		int multiplesCount = 0;
+		while (num <= limit) {
+			// Check if num is a multiple of 3
+			if (num % 3 == 0) {
+				multiplesCount ++;
+			}
+			num ++;
+		}
+		System.out.println("There are " + multiplesCount + " multiples of 3 <=15");
+		System.out.println();
+		
+		System.out.println("Do-While Loops:");
+		// Do-While Loop (or Do Loop) - execute once before checking the condition
+		tries = 0;
+		// Check that the limit is valid before we begin
+		if ((limit >= 1) && (limit <= 1000)) {
+			int guess;
+			do {
+				guess = rand.nextInt(1000) + 1;
+				tries ++;
+			} while (guess != limit);
+			System.out.println("It took " + tries + " tries to guess " + limit);
+		}
+		System.out.println();
+	
+		// For Loops - iterate a set number of times
+		System.out.println("For Loop:");
+		// for (initialization; condition; update)
+		for (int ii=1; ii<=limit; ii++) {
 			System.out.print(" " + ii);
 		}
 		System.out.println();
-
-		// For 1 to 10
-		for (int ii=1; ii<=10; ii++) {
-			System.out.print(" " + ii);
+		
+		
+		// Nested Loops
+		// Times table
+		System.out.println("Times Table");
+		for (int ii=1; ii<=limit; ii++) {
+			for (int jj=1; jj<=limit; jj++) {
+				System.out.printf("%4d", ii*jj);
+			}
+			System.out.println();
 		}
 		System.out.println();
 		
-		// Nested loops
-		System.out.println();
-		System.out.println("Nested loop:");
+		// Drawing shapes
 		// if limit is even, print a square, if it is odd print a triangle
-		if (limit % 2 == 0) {   // limit is even
-			for(int row=1; row<=5; ++row) {
-				for (int col=1; col<=5; ++col) {
-					System.out.print("*");
+		if (limit % 2 == 0) {
+			for (int row=1; row <= limit; ++row) {
+				for (int col=1; col <= limit; ++col) {
+					System.out.print('*');
+				}
+				System.out.println();
+			}	
+		} else {
+			for (int row=1; row <= limit; ++row) {
+				for (int col=row; col <= limit; ++col) {
+					System.out.print('*');
 				}
 				System.out.println();
 			}
 		}
-		else {  // limit is odd
-			for(int row=1; row<=5; ++row) {
-				for (int col=1; col<=5; ++col) {
-					System.out.print("*");
-				}
-				System.out.println();
-			}
-		}
-
-
+		
+		scan.close();
+	
 	}
-
 }
