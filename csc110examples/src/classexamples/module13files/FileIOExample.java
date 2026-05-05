c10feb57ea2400ccf89994893b7600546e09ac51
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2026-05-05 12:38:21 -0700

    Spring 2026 Examples

--- csc110examples/src/classexamples/module13files/FileIOExample.java
+++ csc110examples/src/classexamples/module13files/FileIOExample.java
@@ -9,45 +9,48 @@ public class FileIOExample {
 		
 		String inputFileName = "src\\classexamples\\module13files\\input.txt";
 		String outputFileName = "src\\classexamples\\module13files\\output.txt";
+		
 		Scanner inFile = null;
-		String lineFromFile;
 		PrintWriter outFile = null;
+		String lineFromFile;
 		int lineNo = 1;
-
+		
 		// Open a file for reading
 		try {
 			inFile = new Scanner(new FileReader(inputFileName));
-		} catch (FileNotFoundException e) {
-			System.out.println("FILE NOT FOUND: " + inputFileName);
-			e.printStackTrace();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("INPUT FILE NOT FOUND: " + inputFileName);
 			System.exit(-1);
 		}
 		
 		// Open a file for writing
 		try {
 			outFile = new PrintWriter(outputFileName);
-		} catch (FileNotFoundException e) {
-			System.out.println("FILE NOT FOUND: " + outputFileName);
-			e.printStackTrace();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("OUTPUT FILE NOT FOUND: " + outputFileName);
 			System.exit(-1);
 		}
-		
-		// Read the input file & write to output file
-		// We should be checking for exceptions here!
-		while(inFile.hasNextLine()) {
-			lineFromFile = inFile.nextLine();
-			outFile.println(lineNo + "(" + lineFromFile.length() + "): " + lineFromFile);
 
+		
+		// Read the input file
+		// We should be checking for exceptions here
+		while (inFile.hasNextLine() ) {
+			lineFromFile = inFile.nextLine();
+			outFile.println(lineNo + ": " + lineFromFile);
 			lineNo++;
 		}
-
-		// Close the file
+		
+		
+		// Close the files
 		if (inFile != null) {
 			inFile.close();
 		}
 		if (outFile != null) {
 			outFile.close();
 		}
+		
 	}
 
 }
