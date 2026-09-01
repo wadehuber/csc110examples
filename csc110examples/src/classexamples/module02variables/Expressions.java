3f1e8d84084d2063553288b013f978520303c756
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2026-09-01 12:41:22 -0700

    Fall 2026 Examples

--- csc110examples/src/classexamples/module02variables/Expressions.java
+++ csc110examples/src/classexamples/module02variables/Expressions.java
@@ -1,42 +1,40 @@
 package classexamples.module02variables;
-
 import java.util.Scanner;
 
 public class Expressions {
-	
-	public static void main (String args[]) {
-	
-		System.out.println("Expressions:");
+
+	public static void main(String[] args) {
+
 		int num;
 		num = 3 + 4;
-		System.out.println("\tExample 1: " + num);
-		System.out.println("\tExample 2: " + 3 + 4);       // concatenation
-		System.out.println("\tExample 3: " + (3 + 4));     // addition
-		System.out.println("\tExample 4: " + (13 / 4));    // integer division
-		System.out.println("\tExample 4: " + (13 / 4.0));  // division
-
+		
+		System.out.println("Expressions:");
+		System.out.println("\tnum = " + num);
+		System.out.println("\tExample 2: \"3\" + \"4\" = " + 3 + 4);       // concatenation
+		System.out.println("\tExample 3: 3 + 4 = " + (3 + 4));             // addition
+		System.out.println("\tExample 4: 13 / 4 = " + (13 / 4));           // integer division
+		System.out.println("\tExample 5: 13 % 4 = " + (13 % 4));           // modulus (remainder)
+		System.out.println("\tExample 6: 13 / 4.0 = " + (13 / 4.0));       // division
+		
 		int a = 7;
 		int b = 3;
-		
 		double m = .99;
 		double n = 12345 / 678.0;
 		
 		System.out.println("a = " + a + " b = " + b);
 		System.out.println("m = " + m + " n = " + n);
-		
+
 		int x = a * a + 4 * b;
 		System.out.println("a * a + 4 * b = " + (a * a + 4 * b));
 		System.out.println("x = " + x);
 		System.out.println("x * 10 = " + (x * 10));
-		
-		x = x * x;
 		System.out.println("x = " + x);
 		
 		// Constants
 		System.out.println("\nConstants:");
 		final int MAX_NUMBER = 4;
 		System.out.println("\tMAX_NUMBER = " + MAX_NUMBER);
-		//MAX_NUMBER = 10;  // Not allowed = MAX_NUMBER is a constant (final)
+		// MAX_NUMBER = 10;  // Not allowed = MAX_NUMBER is a constant (final)
 		
 		int value1 = 5;
 		int value2 = 7;
@@ -54,12 +52,13 @@ public class Expressions {
 
 		// Reading input from the user
 		Scanner scan = new Scanner(System.in);
-		System.out.println("\nEnter a number: ");
+		System.out.print("\nEnter a number: ");
 		
 		int user = scan.nextInt();
 		System.out.println("You entered a " + user);
 
 		scan.close();
+	
 	}
 
 }
--- csc110examples/src/classexamples/module02variables/Variables.java
+++ csc110examples/src/classexamples/module02variables/Variables.java
@@ -1,37 +1,40 @@
-// Class examples for CSC110 Module 2 - Data & Expressions
-// 27-Aug-2024
-
 package classexamples.module02variables;
-
-import java.util.Scanner;
 
 public class Variables {
 
 	public static void main(String[] args) {
+		// Strings
 		System.out.print("CSC110 ");
 		System.out.println("Module 2");
 		
-		//String concatenation
-		System.out.println("Chandler" + "-" + "Gilbert");
+		System.out.println("String" + "Concatenation");
+		System.out.println(4 + "Count=" + 2 + 8);
 		
+		System.out.println("When one door closes, another opens; but we often " 
+				+ "look so long and so regretfully upon the closed door that we do "
+				+ "not see the one which has opened for us.");
+		
+		// Escape sequences
 		// \n : newline
 		// \t : tab
 		// \" : double-quote
+		// \\ : backslash
 		System.out.println("The greatest enemy of knowledge is not \n" 
 				+ "\tignorance, it is the illusion of \n" 
 				+ "\tknowledge. - Stephen Hawking");
-		//System.out.println(""To be, or not to be"");
+		// System.out.println(""To be, or not to be"");
 		System.out.println("\"To be, or not to be\"");
 		
 		// Be careful with editors that have smart quotes!
 		// Syntax error on token "Invalid Character", @ expected
 		// System.out.println(“This is text copied from Google docs”);
-
+		System.out.println("This is text copied from Google docs");
+		
 		// Variables
 		int value1;      // Declare a variable (with a bad name!)
-		int value2 = 5;  // Declare & initialize a variable
+		int value2 = 5;  // Declare and initialize a variable
 		
-		value1 = 2;      //  Set a variable's value
+		value1 = 2;      // Set a variable's value
 		
 		System.out.println("value1 = " + value1);
 		System.out.println("value2 = " + value2);
@@ -39,28 +42,28 @@ public class Variables {
 		// Be careful with + : sometimes addition, sometimes concatenation!
 		System.out.println("\"value1 + value1\" = " + value1 + value2);
 		System.out.println("\"(value1 + value1)\" = " + (value1 + value2));
-
+		
 		// Primitive data type
 		int number = 10;
 		long bigNumber = 1234567890L;  // L indicates this is a long literal 
 		double realNumber = 3.14159;   // floating-point number
 		boolean decision = true;       // true or false
 		char letter = 'A';             // single character in single quotes
-		
+		char backslash = '\\';         // single (escaped) character in single quotes
+
 		System.out.println("\nPrimitive types");
 		System.out.println("\tint number = " + number);
 		System.out.println("\tlong bigNumber = " + bigNumber);
 		System.out.println("\tdouble realNumber = " + realNumber);
 		System.out.println("\tboolean decision = " + decision);
 		System.out.println("\tchar letter = " + letter);
-		
+
 		// char is a numeric type
 		letter = 'A' + 1;
 		System.out.println('A' + 1);
 		System.out.println(letter);
-		letter = 'A' + '1';
-		System.out.println(letter);
-
+		
+		
 	}
 
 }
