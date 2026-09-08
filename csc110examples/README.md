a57011f80e0d1d8573238f584f90bb9a253acb24
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2026-09-08 10:55:31 -0700

    Fall 2026 updates

--- csc110examples/README.md
+++ csc110examples/README.md
@@ -4,7 +4,7 @@ Code examples and videos for CSC110 - Introduction to Computer Science (Java)
 
 This repository contains
 
-- [Code examples & videos](./src/videoexamples)
+- [Code examples & videos](./src/codeexamples)
 - [In-class code](./src/classexamples)
 - [Project starter code](./src/projects)
 - [Starter code for select Eclipse labs](./src/labs)
--- csc110examples/src/classexamples/module03objects/ClassesAndObjects.java
+++ csc110examples/src/classexamples/module03objects/ClassesAndObjects.java
@@ -5,46 +5,60 @@ import java.util.Random;
 public class ClassesAndObjects {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Primitive variables");
-		int num = 10;       // 10 is an integer literal
-		double pi = 3.14;   // 3.14 is a double literal
-		char grade = 'A';   // 'A' is a character literal
+		int num = 10; // 10 is an integer literal
+		double pi = 3.14; // 3.14 is a double literal
+		char grade = 'A'; // 'A' is a character literal
 		System.out.print("Values: num=" + num + ", pi=" + pi);
 		System.out.println(", grade=" + grade);
 		System.out.println();
-		
+
 		System.out.println("Object reference variables");
-		String subject = "Computer Science";  // "Computer Science" is a string literal
-		String movie = new String("Interstellar");  // Instantiate a string
-		System.out.println("subject = \"" + subject + "\"");
-		System.out.println("movie = \"" + movie + "\"");
-		
-		// Object methods
-		System.out.println("The string \"" + movie + "\" is " +
-		                    movie.length() + " characters long");
-		
-		// The Random class
+		String movie = "Spider-Man: No Way Home";
+		int len = movie.length();
+		System.out.println("len = " + len);
+		System.out.println("movie.length() + 1 = " + (movie.length() + 1));
+
+		String subject = "Computer Science"; // "Computer Science" is a string literal
+		len = subject.length();
+		System.out.println("len = " + len);
+		System.out.println("subject.length() + 1 = " + (subject.length() + 1));
+
+		// The Random Class
 		Random rand = new Random();
-		System.out.println("Using a Random object");
-		System.out.println("Random integer: " + rand.nextInt());
-		System.out.println("Random integer (0-9): " + (rand.nextInt(10)));
-		System.out.println("Random integer (1-10): " + (rand.nextInt(10)+1));
-		System.out.println("Random double: " + rand.nextDouble());
-		System.out.println("Random long: " + rand.nextLong());
+		System.out.println("Using a Random object:");
+		System.out.println("\tRandom integer: " + rand.nextInt());
+		System.out.println("\tRandom integer [0-9]: " + rand.nextInt(10));
+		System.out.println("\tRandom integer [1-10]: " + (rand.nextInt(10) + 1));
+		System.out.println("\tRandom double: " + rand.nextDouble());
+		System.out.println("\tRandom long: " + rand.nextLong());
 		System.out.println();
-	
-		// Static methods in Math
-		System.out.println("Using static methods from the Math class");
+
+		Random rand2 = new Random(13); // Seed value
+		System.out.println("\tRandom integer [1-10]: " + (rand2.nextInt(10) + 1));
+		System.out.println("\tRandom integer [1-10]: " + (rand2.nextInt(10) + 1));
+		System.out.println("\tRandom integer [1-10]: " + (rand2.nextInt(10) + 1));
+
+		// The Math class (static methods)
+		System.out.println("\nUsing static methods from the Math class:");
+		System.out.println("\tSquare root of 25 = " + Math.sqrt(25));
+		System.out.println("\tSquare root of 42 = " + Math.sqrt(42));
+		System.out.println("2 raised to the power of 8 = " + Math.pow(2, 8));
+		System.out.println("3 raised to the power of 4 = " + Math.pow(3, 4));
+		System.out.println("7 raised to the power of 5 = " + Math.pow(7, 5));
+		System.out.println("11 raised to the power of 12 = " + Math.pow(11, 12));
 		System.out.println("PI = " + Math.PI);
 		System.out.println("cos(pi) = " + Math.cos(Math.PI));
 		System.out.println("sin(pi) = " + Math.sin(Math.PI));
 		System.out.println("sin(45 deg) = " + Math.sin(Math.toRadians(45)));
-		System.out.println("square root of 25 = " + Math.sqrt(25));
-		System.out.println("square root of 42 = " + Math.sqrt(42));
-		System.out.println("2 raised to the power of 8 = "+ Math.pow(2,  8));
-		System.out.println("7 raised to the power of 3 = "+ Math.pow(7,  3));
-		
+		System.out.println("Absolute value of 10 = " + Math.abs(10));
+		System.out.println("Absolute value of -10 = " + Math.abs(-10));
+		System.out.println("Rounding 12.34 = " + Math.round(12.34));
+		System.out.println("Rounding 56.78 = " + Math.round(56.78));
+		System.out.println("Maximum of 10 & 20 = " + Math.max(10, 20));
+		System.out.println("Minimum of 10 & 20 = " + Math.min(10, 20));
+
 	}
 
 }
