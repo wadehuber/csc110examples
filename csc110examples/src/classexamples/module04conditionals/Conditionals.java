2d88582dcd3de82ff35c9981ce0c0dc2a21b6d05
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2026-09-15 11:39:43 -0700

    Merge branch 'master' of https://github.com/wadehuber/csc110examples.git

--- csc110examples/src/classexamples/module04conditionals/Conditionals.java
+++ csc110examples/src/classexamples/module04conditionals/Conditionals.java
@@ -14,69 +14,73 @@ public class Conditionals {
 		System.out.println("Examples of conditionals in Java");
 
 		System.out.println("Java if statements");
-		
-		// If num1 is less than num2 print "Hello"
+		// If num1 is less than num2 print Hello!
 		if (num1 < num2) {
 			System.out.println("Hello!");
 		}
-		// If num1 is greater than num2 print "YOU FAIL"
+
+		// If num1 is bigger than num2 print YOU FAIL!
 		if (num1 > num2) {
 			System.out.println("YOU FAIL!");
 		}
-		
+
 		// Multiple conditions
 		// If num3 > num2 AND if num3 is a multiple of 3
 		if ((num3 > num2) && (num3 % 3 == 0)) {
 			System.out.println("It is both");
 		}
-		System.out.println("Done with simple if statements");
-		System.out.println();
-
-		System.out.println("Java if-else statements");
 		
-		// If num1 + num2 is less than num3, print "Greetings"
-		//   otherwise print "You are not welcome"
-		if ((num1 + num2) < num3) {
-			System.out.println("Greetings");
-			System.out.println("   Thanks for coming!");
-		}
-		else {
+		System.out.println();
+		System.out.println("Java if-else statements");
+		// If num1 + num2 is less than num3 print Greetings!
+		//  otherwise, print "You are not welcome"
+		if ((num1+num2) < num3) {
+			System.out.println("Greetings!");
+			System.out.println("Thanks for coming");
+		} else {
 			System.out.println("You are not welcome");
 		}
 		
 		// If the square of num1 is greater than num2 * num3
-		//    display "You many continue"
-		//    otherwise, display "You shall not pass!"
-		if (Math.pow(num1,  2) > (num2 * num3)) {
+		//   display "You may continue"
+		//   otherwise, display "You shall not pass!"
+		if (Math.pow(num1,2) > (num2 * num3)) {
 			System.out.println("You may continue");
 		} else {
-			System.out.println("You may continue");
+			System.out.println("You shall not pass!");
 		}
 		
 		System.out.println();
-		System.out.println("Best practice use braces even with a 1 line code block");
-		int guessed = 10;
-		if (num1 == guessed)  {
+		System.out.println("Best practice: use braces even with a 1 line code block");
+		int secretNumber = 42;
+		if(num1 == secretNumber) {
 			System.out.println("You are the winner!");
 			System.out.println("Here is a million dollars!");
 		}
-			
+		
+		System.out.println();
+		System.out.println("Working with Strings");
+
 		String p = new String("Percy");
 		String p2 = "Percy";
 		
-		System.out.println("\nStrings:");
 		System.out.println("\tstr1 = " + str1);
 		System.out.println("\tstr2 = " + str2);
 		System.out.println("\tstr3 = " + str3);
 		System.out.println("\tp = " + p);
 		System.out.println("\tp2 = " + p2);
-		System.out.println();
-
+			
 		System.out.println("Comparing \"" + str1 + "\" with \"" + p + "\"");
+		
+		System.out.print("\twith == : " );
+		if(str1 == p2) {
+			System.out.println("Same name");
+		}
+		else {
+			System.out.println("Different name");
+		}
 
-		// This compares that the object reference variables are referring to 
-		//  the same object, NOT that they have the same value
-		System.out.print("\twith == : ");
+		System.out.print("\twith == : " );
 		if(str1 == p) {
 			System.out.println("Same name");
 		}
@@ -84,7 +88,7 @@ public class Conditionals {
 			System.out.println("Different name");
 		}
 
-		System.out.print("\twith .equals : ");
+		System.out.print("\twith equals() : " );
 		if(str1.equals(p)) {
 			System.out.println("Same name");
 		}
@@ -93,13 +97,11 @@ public class Conditionals {
 		}
 		
 		// Use .toLowerCase when comparing strings to ignore case
-		if (str3.toLowerCase().equals("GrOVeR".toLowerCase())) {
+		if (str3.toLowerCase().equals("GrOveR".toLowerCase())) {
 			System.out.println("Grover is ready");
-		}
-		else {
+		} else {
 			System.out.println("Grover is not here");
 		}
-		
 		// Use compareTo to compare the ordering of two strings
 		if (str1.compareTo(str2) > 0 ) {
 			System.out.println(str1 + " is after " + str2 + "(compareTo = " + str1.compareTo(str2) + ")");
@@ -145,10 +147,12 @@ public class Conditionals {
 		}
 		
 		System.out.println();
+		num2=10;
 		System.out.println("num2 = " + num2);
 		switch(num2) {
 			case 10:
 			case 15:
+				System.out.println("Its a one fiver");
 			case 20:
 			case 30:
 				System.out.println("Full access");
@@ -157,6 +161,7 @@ public class Conditionals {
 				System.out.println("Limited access");
 				break;
 		}
+
 	
 	}
 }
