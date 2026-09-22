1c00824278040c434d71ea1103defa63594132b5
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2026-09-22 11:25:50 -0700

    Fall 2026 examples

--- csc110examples/src/classexamples/module05loops/Loops.java
+++ csc110examples/src/classexamples/module05loops/Loops.java
@@ -10,42 +10,43 @@ public class Loops {
 		Random rand = new Random();
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter a number: ");
-		//int limit = scan.nextInt();
-		int limit = 9;
+		int limit = scan.nextInt();
+		//int limit = 9;
 		
 		// While Loop - repeats while a condition is true
-		System.out.println("While loop:");
 		int num = 1;
 		while (num <= limit) {
 			System.out.print(" " + num);
-			++num;
+			num ++;
 		}
+		System.out.println();
 		System.out.println();
 		
 		// Keep a running total
-		System.out.println("While Loop - running total:");
+		System.out.println("While loop - running total:");
 		num = 1;
 		int total = 0;
-		while (num <= limit) {
+		while(num <= limit) {
 			total += num;
-			++num;
+			num ++;
 		}
-		System.out.println("The total is: " + total);
+		System.out.println("The total is : " + total);
 		System.out.println();
-		
-		// While Loop until a condition is met
+
+		// Loop only if a condition is met
 		int tries = 1;
-		// Check that the limit is valid before we begin
-		if ((limit >= 1) && (limit <= 1000)) {
+		int secretNum = limit;
+		if ((secretNum >= 1) && (secretNum <= 1000)) {
 			int guess = rand.nextInt(1000) + 1;
-			while (guess != limit) {
+			while (guess != secretNum) {
 				tries ++;
 				guess = rand.nextInt(1000) + 1;
 			}
-			System.out.println("It took " + tries + " tries to guess " + limit);
+			System.out.println("It took " + tries + " to guess " + secretNum);
 		}
-		
-		// Nested conditional in a loop
+		System.out.println();
+			
+		// Nested condition in a loop
 		num = 1;
 		int multiplesCount = 0;
 		while (num <= limit) {
@@ -55,62 +56,72 @@ public class Loops {
 			}
 			num ++;
 		}
-		System.out.println("There are " + multiplesCount + " multiples of 3 <=15");
+		System.out.println("There are " + multiplesCount + " multiples of 3 <=" + limit);
 		System.out.println();
 		
-		System.out.println("Do-While Loops:");
-		// Do-While Loop (or Do Loop) - execute once before checking the condition
+		int value = -1;
+		total = 0;
+		System.out.print("Enter a positive number (0 to quit): ");
+		value = scan.nextInt();
+		while (value != 0) {
+			total += value;
+			System.out.println("  The total so far is " + total);
+			System.out.print("Enter a positive number (0 to quit): ");
+			value = scan.nextInt();
+		}
+		System.out.println("The total is " + total);
+
+		
+		System.out.println("Do-while loops:");
+		// Same thing, but with a do-while
+		value = -1;
+		total = 0;
+		do {
+			System.out.print("Enter a positive number (0 to quit): ");
+			value = scan.nextInt();
+			total += value;
+			System.out.println("  The total is " + total);
+		} while (value != 0);
+		System.out.println();
+		
+		// do-while - execute once before checking the condition
 		tries = 0;
-		// Check that the limit is valid before we begin
-		if ((limit >= 1) && (limit <= 1000)) {
+		if ((secretNum >= 1) && (secretNum <= 1000)) {
 			int guess;
 			do {
 				guess = rand.nextInt(1000) + 1;
 				tries ++;
-			} while (guess != limit);
-			System.out.println("It took " + tries + " tries to guess " + limit);
+			} while (guess != secretNum);
+			System.out.println("It too " + tries + " tries to guess the secret number " + secretNum);
+
 		}
 		System.out.println();
-	
+
 		// For Loops - iterate a set number of times
-		System.out.println("For Loop:");
-		// for (initialization; condition; update)
-		for (int ii=1; ii<=limit; ii++) {
-			System.out.print(" " + ii);
+		System.out.println("");
+		for(int ii = 1; ii<=limit; ii++) {
+			System.out.print(" " + ii) ;
 		}
 		System.out.println();
-		
-		
-		// Nested Loops
-		// Times table
-		System.out.println("Times Table");
-		for (int ii=1; ii<=limit; ii++) {
-			for (int jj=1; jj<=limit; jj++) {
-				System.out.printf("%4d", ii*jj);
-			}
-			System.out.println();
-		}
 		System.out.println();
 		
 		// Drawing shapes
-		// if limit is even, print a square, if it is odd print a triangle
-		if (limit % 2 == 0) {
-			for (int row=1; row <= limit; ++row) {
-				for (int col=1; col <= limit; ++col) {
-					System.out.print('*');
-				}
-				System.out.println();
-			}	
-		} else {
-			for (int row=1; row <= limit; ++row) {
-				for (int col=row; col <= limit; ++col) {
-					System.out.print('*');
-				}
-				System.out.println();
+		// Square
+		for (int row=1;row<limit;row++) {
+			for (int col=1;col<limit;col++) {
+				System.out.print("* ");
 			}
+			System.out.println();
 		}
 		
-		scan.close();
-	
+		// Triangle
+		for (int row=1;row<limit;row++) {
+			for (int col=1;col<row;col++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		
 	}
 }
